95d327b4a16fe81d732007b50f1c64ffd989a84e
alpeshl <49274347+alpeshl@users.noreply.github.com>
2023-07-05 19:57:10 +0530

    sste

--- src/App.tsx
+++ src/App.tsx
@@ -37,6 +37,7 @@ import "./customize.scss";
 import "./override.scss";
 
 const Context = React.createContext({ name: "Default" });
+
 const exceptionRoutes = ["/", "/invest", "/feedback"];
 
 const App = () => {
